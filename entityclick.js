626d3728c8c0496c210365a101eba28ba261a54b
milad <milad@highfidelity.io>
2018-03-28 13:18:07 -0700

    change to mousepress

--- entityclick.js
+++ entityclick.js
@@ -11,9 +11,11 @@
             print("userData=", JSON.stringify(userData));
             message = userData.message;
         },
-        clickDownOnEntity: function () {
+        mousePressOnEntity: function () {
             print("### I just clicked");
-            Messages.sendMessage("Messages_Control_Channel", JSON.stringify({type: message.type}));
+            var messageToSend = JSON.stringify({type: message.type});
+            print("message to send: ", messageToSend);
+            Messages.sendMessage("Messages_Control_Channel", messageToSend);
         },
         unload: function(entityID) {
             
