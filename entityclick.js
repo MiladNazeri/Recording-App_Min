615d0391aefe0b2ba63697510723e9fc3b7ebf50
milad <milad@highfidelity.io>
2018-03-28 13:12:52 -0700

    added quick logs

--- entityclick.js
+++ entityclick.js
@@ -8,9 +8,11 @@
     Play.prototype = {
         preload: function(entityID) {
             var userData = JSON.parse(Entities.getEntityProperties(entityID, 'userData').userData);
+            print("userData=", JSON.stringify(userData));
             message = userData.message;
         },
-        clickDownOnEntity: function () { 
+        clickDownOnEntity: function () {
+            print("### I just clicked");
             Messages.sendMessage("Messages_Control_Channel", JSON.stringify({type: message.type}));
         },
         unload: function(entityID) {
